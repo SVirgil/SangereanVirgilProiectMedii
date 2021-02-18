de0e8e0557fb8e447c43343e10b308fce37f361d
Virgil Sangerean <virgilsangerean@gmail.com>
2021-02-18 21:08:03 +0200

    1st commit

--- Migrations/20210218190523_DataLansarii.Designer.cs
+++ Migrations/20210218190523_DataLansarii.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SangereanVirgilProiectMedii.Data;
 
 namespace SangereanVirgilProiectMedii.Migrations
 {
     [DbContext(typeof(SangereanVirgilProiectMediiContext))]
-    partial class SangereanVirgilProiectMediiContextModelSnapshot : ModelSnapshot
+    [Migration("20210218190523_DataLansarii")]
+    partial class DataLansarii
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
